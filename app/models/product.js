2e7d09435b30f03a0b3fbe6eef3ce48d2a73003b
saiteja0604 <saiteja.munagala@eunimart.com>
2021-07-29 11:00:51 +0530

    Deployment

--- app/models/product.js
+++ app/models/product.js
@@ -1,3 +1,16 @@
-let Product=[]
+let Product=[
+    {
+        "id": 1,
+        "productName": "OnePLus",
+        "productType": "Mobile"
+    },
+
+    {
+        "id": 2,
+        "productName": "Iphone 12 pro max",
+        "productType": "Mobile"
+    }
+
+]
 
 module.exports=Product;
